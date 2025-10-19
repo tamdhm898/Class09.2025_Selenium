2041b2a08d1466bd75f02a9f6af3f7aa096fb6c1
Your Name <tam.dhm898@gmail.com>
2025-10-19 17:43:03 +0200

    BTVN_DAY7

---
+++ Automation/src/main/java/automation/common/CommonBase.java
@@ -0,0 +1,10 @@
+package automation.common;
+
+public class CommonBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Automation/src/main/java/automation/testsuit/Practice.java
@@ -0,0 +1,28 @@
+package automation.testsuit;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void tinhTongMang() {
+		// Nhập vào một mảng gồm n phần tử
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập độ dài mảng: ");
+		int n = sc.nextInt();
+
+		int[] a = new int[n];
+		int sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Nhập phần tử mảng thứ " + (i + 1) + ": ");
+			a[i] = sc.nextInt();
+			sum += a[i];
+		}
+
+		System.out.println("Tổng của mảng là: " + sum);
+		sc.close();
+	}
+}

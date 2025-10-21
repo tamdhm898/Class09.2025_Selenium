596387777e04332060b92eb37b785be6c04abcef
Your Name <tam.dhm898@gmail.com>
2025-10-21 22:42:56 +0200

    BTVN_Day7

--- Automation/src/main/java/automation/testsuit/Practice.java
+++ Automation/src/main/java/automation/testsuit/Practice.java
@@ -1,3 +1,4 @@
+
 package automation.testsuit;
 
 import java.util.Scanner;
@@ -7,22 +8,27 @@ import org.testng.annotations.Test;
 public class Practice {
 
 	@Test
-	public void tinhTongMang() {
+	public void timSoNhoNhatMang() {
 		// Nhập vào một mảng gồm n phần tử
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Nhập độ dài mảng: ");
 		int n = sc.nextInt();
 
 		int[] a = new int[n];
-		int sum = 0;
-
 		for (int i = 0; i < n; i++) {
 			System.out.print("Nhập phần tử mảng thứ " + (i + 1) + ": ");
 			a[i] = sc.nextInt();
-			sum += a[i];
 		}
 
-		System.out.println("Tổng của mảng là: " + sum);
+		// Tìm phần tử nhỏ nhất
+		int min = a[0];
+		for (int i = 1; i < n; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+
+		System.out.println("Số nhỏ nhất trong mảng là: " + min);
 		sc.close();
 	}
 }
